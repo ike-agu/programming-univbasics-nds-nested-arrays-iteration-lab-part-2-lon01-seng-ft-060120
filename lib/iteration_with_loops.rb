4da9899f597ab1ec12201491163edea0f64816ab
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-21 17:26:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,6 +12,8 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     if src[row_index][element_index].min
      new_array << src[row_index][element_index]
+     
+   end 
     element_index +=1 
     
   end 
